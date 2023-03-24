33818839a76101326a84e34b64295f490c4b9ed0
shri raghavi <shriraghavichandrasekaran@gmail.com>
2023-03-24 20:45:09 +0530

    Initial Commit

---
+++ gyanbee/gyanbeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  gyanbeeApp.swift
+//  gyanbee
+//
+//  Created by SHRI RAGHAVI CHANDRASEKARAN on 24/03/23.
+//
+
+import SwiftUI
+
+@main
+struct gyanbeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
